504e0cc0e7f3e50016906dd9f1e16f21ba0cef32
David Morton <davidmorton0@gmail.com>
2020-02-17 14:27:05 +0000

    Updates

---
+++ precache-manifest.95c5fb5aec478cc0ca0049f80921bae7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dff319d6d0e4d8156797a60c5b29ade5",
+    "url": "/an-app/index.html"
+  },
+  {
+    "revision": "07778c5f3cb6dc60da32",
+    "url": "/an-app/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "744698111b775f55aefe",
+    "url": "/an-app/static/js/2.73f7096c.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/an-app/static/js/2.73f7096c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07778c5f3cb6dc60da32",
+    "url": "/an-app/static/js/main.75d82a8d.chunk.js"
+  },
+  {
+    "revision": "021572781adf1d09e098",
+    "url": "/an-app/static/js/runtime-main.302f0c3b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/an-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
