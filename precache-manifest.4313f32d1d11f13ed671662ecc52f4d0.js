c125a4a4e92c959c658cfea10b23bdf3bc61705a
David Morton <davidmorton0@gmail.com>
2020-02-17 15:46:07 +0000

    Updates

---
+++ precache-manifest.4313f32d1d11f13ed671662ecc52f4d0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7088baae25afc44e2e1d8bc0c0d9b8b9",
+    "url": "/random-quote-machine/index.html"
+  },
+  {
+    "revision": "80dea7db17253985c89c",
+    "url": "/random-quote-machine/static/css/main.929f41e9.chunk.css"
+  },
+  {
+    "revision": "d01e3944cf65b954c898",
+    "url": "/random-quote-machine/static/js/2.e1266fe0.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/random-quote-machine/static/js/2.e1266fe0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80dea7db17253985c89c",
+    "url": "/random-quote-machine/static/js/main.d5ffc9df.chunk.js"
+  },
+  {
+    "revision": "afc9b93c50eb3e83906a",
+    "url": "/random-quote-machine/static/js/runtime-main.daced29d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/random-quote-machine/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/an-app/precache-manifest.fbf47d72536ef9734c3efffb6d5651e8.js"
+  "/random-quote-machine/precache-manifest.4313f32d1d11f13ed671662ecc52f4d0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/an-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/random-quote-machine/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
